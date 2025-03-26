754de4382dca07bd305e9c7217dc047b65758c8a
mimiprosper <mimiprosper@hotmail.com>
2025-03-26 17:11:24 +0100

    first comit

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,9 @@
 # Created a SegWit address.
+bitcoin-cli -regtest getnewaddress "segwit_address" bech32
+
 # Add funds to the address.
+bitcoin-cli -regtest generatetoaddress 101 "bcrt1qxyz..."
+
 # Return only the Address
+bitcoin-cli -regtest getnewaddress
+
